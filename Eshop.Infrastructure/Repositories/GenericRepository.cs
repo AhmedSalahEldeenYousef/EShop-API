367fdb067e3d6ee2b9b5b78d3a321a27db2dcab4
ahmed salah <ahmedsallah415@gmail.com>
2025-05-28 18:26:45 +0300

    Adding Project Files

---
+++ Eshop.Infrastructure/Repositories/GenericRepository.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Eshop.Core.Interfaces;
+
+namespace Eshop.Infrastructure.Repositories
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        public Task AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task DeleteAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IReadOnlyList<T>> GetAllAsync(params System.Linq.Expressions.Expression<Func<T, object>>[] includs)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> GetByIdAsync(int id, params System.Linq.Expressions.Expression<Func<T, object>>[] includes)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Eshop.core/Entities/Product/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Eshop.Core.Entities.Product
+{
+    public  class Product : BaseEntity<int>
+    {
+        public string ProductName { get; set; }
+        public string Description { get; set; }
+        public decimal PriceTotal { get; set; }
+        public int CategoryId { get; set; }
+        //setting relationship
+
+        [ForeignKey(nameof(CategoryId))]
+        public virtual Category Category { get; set; }
+        public virtual List<Photo> photos { get; set; }
+    }
+}
