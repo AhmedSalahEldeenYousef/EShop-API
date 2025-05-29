b4241907e79b49870b539fc77b36a2ae1ac473dd
ahmed salah <ahmedsallah415@gmail.com>
2025-05-29 09:35:01 +0300

    adding s shop connection string

--- Eshop.Infrastructure/InfrastructureRegisteration.cs
+++ Eshop.Infrastructure/InfrastructureRegisteration.cs
@@ -4,23 +4,34 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Eshop.Core.Interfaces;
+using Eshop.Infrastructure.Data;
 using Eshop.Infrastructure.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Eshop.Infrastructure
 {
     public static class InfrastructureRegisteration
     {
-        public static IServiceCollection InfrastructureConfiguration(this IServiceCollection services)
+        public static IServiceCollection InfrastructureConfiguration(this IServiceCollection services,IConfiguration configuration)
         {
-            services.AddScoped(typeof(IGenericRepository<>), typeof(IGenericRepository<>));
-            // services.AddScoped<ICategoryRepository, CategoryRepository>();
-           //  services.AddScoped<IProductRepository, ProductRepository>();
-           //  services.AddScoped<IPhotoRepository, PhotoRepository>();
+            services.AddScoped(typeof(IGenericRepository<>), typeof(GenericRepository<>));
 
+            // services.AddScoped<ICategoryRepository, CategoryRepository>();
+            //  services.AddScoped<IProductRepository, ProductRepository>();
+            //  services.AddScoped<IPhotoRepository, PhotoRepository>();
+
+
+            //Applying Unite Of Work
             services.AddScoped<IUnitOfWork, UnitOfWork>();
 
+            //Applying DbContext 
 
+            services.AddDbContext<AppDbContext>(op =>
+            {
+                op.UseSqlServer(configuration.GetConnectionString("eshopconnectionstring"));
+            });
             return services;
         }
     }
